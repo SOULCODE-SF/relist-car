a575dbe97a719c4956fc2243f0102b7ced6c8c35
lintangknitto <lintangdandungprakoso.knitto@gmail.com>
2024-07-09 09:10:44 +0700

    update

---
+++ stylelint.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: 'stylelint-config-standard', // Use the standard stylelint configuration
+  rules: {
+    // Add your own rules or override existing rules here
+  },
+};
