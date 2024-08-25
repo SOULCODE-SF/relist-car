c88a5afb07fe7950c465a42e73bc70613442cd9c
Lintangeaa <lindprak@gmail.com>
2024-08-25 17:06:09 +0700

    update relist car

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'relist',
+      name: 'relist-car',
       script: './bin/www',
       instances: 1,
-      autorestart: true, 
-      watch: false, 
-      max_memory_restart: '1G', 
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'production', 
+        NODE_ENV: 'production',
       },
-      error_file: './logs/pm2-error.log', 
-      out_file: './logs/pm2-out.log', 
+      error_file: './logs/pm2-error.log',
+      out_file: './logs/pm2-out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      merge_logs: true, 
+      merge_logs: true,
       time: true,
     },
   ],
